bd93b0ebe8247ae44408ddf1a0d6922e4392288b
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-05-28 23:33:13 +0300

    changes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 const corsOptions = {
     origin: 'http://localhost:4200', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Authorization,Origin,X-Requested-With,Content-Type,Accept',
 };
   
 app.use(cors(corsOptions));
