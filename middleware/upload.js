c3d99050f7868fd43ce65f59b2a71d6b13e97f18
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-05-29 11:21:04 +0300

    temp changes

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,27 +1,27 @@
-import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+// const express = require('express');
+// const { put } = require('@vercel/blob');
  
-export async function POST(request) {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
+// export async function POST(request) {
+//   const { searchParams } = new URL(request.url);
+//   const filename = searchParams.get('filename');
  
-  const blob = await put(filename, request.body, {
-    access: 'public',
-  });
+//   const blob = await put(filename, request.body, {
+//     access: 'public',
+//   });
  
-  return NextResponse.json(blob);
-}
-// const multer = require('multer');
+//   return NextResponse.json(blob);
+// }
+// // const multer = require('multer');
 
-// const Storage = multer.diskStorage({
-//     destination: 'uploads',
-//     filename:(req,file,cb) =>{
-//         cb(null,file.originalname);
-//     },
-// });
+// // const Storage = multer.diskStorage({
+// //     destination: 'uploads',
+// //     filename:(req,file,cb) =>{
+// //         cb(null,file.originalname);
+// //     },
+// // });
 
-// const upload = multer({
-//     storage:Storage
-// }).single('testImage')
+// // const upload = multer({
+// //     storage:Storage
+// // }).single('testImage')
 
-module.exports = upload;
+// module.exports = upload;
