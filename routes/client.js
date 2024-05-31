f022a3b30846e70ca8bceefab46eee75ee295483
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-05-31 12:18:26 +0300

    changes

--- routes/client.js
+++ routes/client.js
@@ -54,7 +54,7 @@ router.delete('/remove/:id',verifyAdmin,async(req,res)=>{
 })
 
 // Get all clients
-router.get('/all', verifyAdmin,async (req, res) => {
+router.get('/all',async (req, res) => {
     try {
         let client;
         client = await Client.find().sort({ name: 1 }); // Sort by name in ascending order
