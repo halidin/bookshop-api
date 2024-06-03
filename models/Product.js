b5130284d63ad13aa74f4f9a05626fecf50c410c
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-06-03 15:02:33 +0300

    changes

--- models/Product.js
+++ models/Product.js
@@ -6,11 +6,11 @@ const productSchema = new mongoose.Schema(
         title:{type: String , required: true, unique: true},
         desc:{type: String , required: true, unique: true},
         img: { type: String, required: true},
-        price:{ type: Number, default:false},
+        price:{ type: Number, required:true},
         
     },
     {timestamps:true}
-)
+);
 
 
 module.exports = mongoose.model("Product",productSchema);
