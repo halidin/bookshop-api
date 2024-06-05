8f64f31cd2e000bf545dd617e834c118f31021dd
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-06-05 20:17:25 +0300

    small change

--- models/Product.js
+++ models/Product.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema(
     {
-        title:{type: String , required: true, unique: true},
-        desc:{type: String , required: true, unique: true},
-        img: { type: String, required: true},
-        price:{ type: Number, required:true},
+        title:{type: String, required:true, unique: true},
+        desc: {type: String, required:true, unique: true},
+        img:  {type: String, required:true},
+        price:{type: Number, required:true}
         
     },
     {timestamps:true}
