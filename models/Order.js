08cbd302a9c60cb6db9a369fc16ef12aa64365d7
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-06-05 15:02:56 +0300

    order changes

--- models/Order.js
+++ models/Order.js
@@ -6,16 +6,18 @@ const orderSchema = new mongoose.Schema(
         userId:{type: String , required: true, unique: true},
         products: [
             {
-                productId: {type: String},
-                quantity: {type: Number,default:1},
+                productId: {type: String,},
+                quantity: {type: Number,default:1,},
+                price:{ type: Number},
+                title:{type: String},
+                img: { type: String, required: true}
+                
             },
         ],
-        amount:{type:Number,required:true},
         fullname:{type: String , required: true},
         email:{type: String , required: true, unique: true},
         phonenumber: { type: Number, required: true,unique: true},
         address: {type: Object,required: true},
-        status: {type: String, default:'Pending'},
 
     },
     {timestamps:true}
