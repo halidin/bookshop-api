e63ab00c6a506f4b77edb38977ceec39de9a4041
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-06-06 13:40:22 +0300

    change

--- routes/order.js
+++ routes/order.js
@@ -37,7 +37,7 @@ router.get('/order/:id',verifyTokenAndAuth, async (req, res) => {
 });
 
 // Get all orders
-router.get('/all',verifyAdmin,async (req, res) => {
+router.get('/my/all',async (req, res) => {
     try {
         let order;
         order = await Order.find();// Sort by name in ascending order
